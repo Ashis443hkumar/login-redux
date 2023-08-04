1f0e15726dfeda9c45c46c6c178adc5a3e626178
Ashis443hkumar <112205180+Ashis443hkumar@users.noreply.github.com>
2023-08-04 23:31:36 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ export default function App() {
   
   const[email, setEmail] = useState("")
   const[password, setPassword] = useState("")
+  console.log(email, password)
+
+  const handlerLogin = () =>{
+      console.log(email, password)
+    
+  }
 
   return (
     <div>
       <label htmlFor="">useremail</label>
-      <input type="text" value={email} onChange={(e) =>setEmail(e.target.value)} placeholder="enter your email" />
+      <input type="text" value={email} onChange={(e) =>setEmail(e.target.value)} placeholder="enter your email" required />
       <label htmlFor="">userpassword</label>
       <input type="text" value={password} onChange={(e) =>setPassword(e.target.value)} placeholder="enter your email" />
+      <button onClick={handlerLogin}>submit</button>
     </div>
   );
 }
